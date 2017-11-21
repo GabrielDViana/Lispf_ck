3089e4c7ec20efa6030ded685c9607a54c0c5eff
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-11-21 21:01:32 -0200

    Making the Parser Rules and Printing the Syntactic Tree

--- LF_ast.py
+++ LF_ast.py
@@ -1,4 +1,6 @@
 import argparse
+import pprint
+
 import ox
 
 # Adding args and description for pyhton3 file call on shell
@@ -11,7 +13,7 @@ ARGUMENTS = parser.parse_args()
 # Saves the args (Files) in constants for further use
 INPUT_LF = ARGUMENTS.lfFile
 
-# Opening LF file
+# File manipulation
 FILE_LF = open(INPUT_LF, 'r')
 READ_FILE = FILE_LF.read()
 FILE_LF.close()
@@ -35,4 +37,26 @@ _lexer = ox.make_lexer(lexer_rules)
 def lexer(src):
     return [tk for tk in _lexer(src) if tk.type not in ('WHITESPACE', 'COMMENT') ]
 
-print(lexer(READ_FILE))
+# Generates the token list by reading the file
+TOKEN_LIST = lexer(READ_FILE)
+print("\nToken list:\n" + str(TOKEN_LIST) + "\n")
+
+# Parser rules to be applied in token_list
+parser_rules = [
+    ('expr : OPEN_PAR CLOSE_PAR', lambda x,y: '()'),
+    ('expr : OPEN_PAR term CLOSE_PAR', lambda x,y,z: y),
+    ('term : atom term', lambda x,y: (x,) + y),
+    ('term : atom', lambda x: (x,)),
+    ('atom : expr', lambda x: x),
+    ('atom : NUMBER', lambda x: x),
+    ('atom : KEYWORD', lambda x: x)
+]
+
+# Generates a valid parser
+parser = ox.make_parser(parser_rules, tokens_simple)
+
+# Generates the final AST
+FINAL_AST = parser(TOKEN_LIST)
+
+print('\nGenerated AST:')
+pprint.pprint(FINAL_AST)
