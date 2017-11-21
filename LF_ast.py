ea3ed542e6e856c3e4422acfa929d1a8a41b9f80
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-11-21 20:57:29 -0200

    Restruturing lexer

--- LF_ast.py
+++ LF_ast.py
@@ -4,33 +4,34 @@ import ox
 # Adding args and description for pyhton3 file call on shell
 parser = argparse.ArgumentParser(description='Generate AST for Lispfuck')
 parser.add_argument('lfFile')
-parser.add_argument('-o')
 
 # Creates a namespace object for the args
 ARGUMENTS = parser.parse_args()
 
 # Saves the args (Files) in constants for further use
 INPUT_LF = ARGUMENTS.lfFile
-OUTPUT_TXT = ARGUMENTS.o
 
 # Opening LF file
 FILE_LF = open(INPUT_LF, 'r')
 READ_FILE = FILE_LF.read()
+FILE_LF.close()
 
 # Lexer rules used for lispfuck
 lexer_rules = [
-    ('OPEN_BRACKETS', r'\('),
-    ('CLOSE_BRACKETS', r'\)'),
+    ('OPEN_PAR', r'\('),
+    ('CLOSE_PAR', r'\)'),
     ('NUMBER', r'\d+'),
     ('COMMENT', r';[^\n]*'),
-    ('WORD', r'[-\w]+'),
+    ('KEYWORD', r'[-\w]+'),
     ('WHITESPACE', r'\s+')
 ]
 
-# Generating initial 
+tokens_simple = ['OPEN_PAR', 'CLOSE_PAR', 'NUMBER', 'KEYWORD']
+
+# Generating initial
 _lexer = ox.make_lexer(lexer_rules)
 
-# Removing WHITESPACE and COMMENT from original token list
+# This function removes WHITESPACE and COMMENT from original token list
 def lexer(src):
     return [tk for tk in _lexer(src) if tk.type not in ('WHITESPACE', 'COMMENT') ]
 
